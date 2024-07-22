92af1cce289bc99a01a9fe9a20c8749d8d75e3c4
CharleSs <quansieuquay2013@gmail.com>
2024-07-23 02:05:33 +0700

    no message

--- src/scenes/global/NavBar.jsx
+++ src/scenes/global/NavBar.jsx
@@ -29,7 +29,6 @@ import { ColorModeContext } from "../../theme";
 export const NavBar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
   const auth = useAuth();
   const theme = useTheme();
-  const colorMode = useContext(ColorModeContext);
 
   const navigate = useNavigate();
 
